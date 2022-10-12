ef2880e8c66e1190eed12ffbe2ca54f33483c8a2
shift <teixeira.matheus2626@gmail.com>
2022-10-12 14:07:37 -0300

    (*) - desafio completo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Background, EstilosGlobales, InputDiv } from "./styles";
 
 function App() {
+  const [active, setActive] = useState(false)
+  const agrandar =()=>{
+    if (active === false)setActive(true);
+    else setActive(false)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <EstilosGlobales></EstilosGlobales>
+      <Background>
+        <InputDiv active={active}>
+        <button onClick={agrandar}>
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+        <input type={'text'} placeholder='Buscar...'></input>
+        </InputDiv>
+      </Background>
     </div>
   );
 }
